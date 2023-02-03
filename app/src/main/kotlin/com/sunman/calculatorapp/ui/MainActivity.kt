7e4d74473a6db0aad688ed89b2cafce8ecbe47ef
sunman24 <andrey_nuzhnov.01@mail.ru>
2023-02-04 00:07:26 +0700

    Updated app module

---
+++ app/src/main/kotlin/com/sunman/calculatorapp/ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sunman.calculatorapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import com.sunman.calculatorapp.R
+
+class MainActivity : AppCompatActivity(R.layout.activity_main)
